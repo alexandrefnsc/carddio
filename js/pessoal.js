bee8825100930af4a108dbbb1437277adb6119f9
alexandrefnsc <alexandre.fnsc@gmail.com>
2014-09-03 11:17:27 -0300

    first commit

---
+++ js/pessoal.js
@@ -0,0 +1,11 @@
+//Meu código de JQuery Pessoal
+$(document).ready(function(){
+	//-------------------------- HOME
+
+	$('.itens-servicos').hover(function() {
+		$(this).css('background', '#0aa9de');
+	}, function() {
+		$(this).css('background', '#0099cc');
+	});
+
+});
