c183c68a95c2a2ba9062db42c27099f4b904dbcc
alexandrefnsc <alexandre.fnsc@gmail.com>
2014-09-05 11:56:57 -0300

    correção de frontend

--- js/pessoal.js
+++ js/pessoal.js
@@ -37,4 +37,83 @@ $(document).ready(function(){
 
 	$('.bxslider').bxSlider();
 
+	// MENU HISTORIA
+	$('#float-menu li').hover(function() {
+		$(this).children('span').css({
+			border: 'solid 2px white',
+			color: '#158fb7',
+			borderRadius:'10px',
+		});
+		$(this).css('cursor', 'pointer');
+	}, function() {
+		$(this).children('span').css({
+			border: 'none',
+			color: '#FFF'
+		});
+	});
+
+	$('#menu-01').click(function(event) {
+		$('#part-historia').animate({
+			top: '0px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-galeria').animate({
+			top: '0px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-trabalhe').animate({
+			top: '0px'
+			}, 1000, 'easeInQuad', function() {
+			confere();
+		});
+	});
+
+	$('#menu-02').click(function(event) {
+		$('#part-historia').animate({
+			top: '-877px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-galeria').animate({
+			top: '-877px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-trabalhe').animate({
+			top: '-877px'
+			}, 1000, 'easeInQuad', function() {
+			confere();
+		});
+	});
+
+	$('#menu-03').click(function(event) {
+		$('#part-historia').animate({
+			top: '-1755px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-galeria').animate({
+			top: '-1755px'
+			}, 1000, 'easeInQuad', function() {
+			/* stuff to do after animation is complete */
+		});
+		$('#part-trabalhe').animate({
+			top: '-1755px'
+			}, 1000, 'easeInQuad', function() {
+			confere();
+		});
+	});
+
+	function confere(){
+		if ($('#part-trabalhe').css('top')=='-1755px') {
+			$('#float-menu').css('color', '#0099cc');
+			$('#float-menu span').css('color', '#0099cc');
+		}else{
+			$('#float-menu').css('color', '#FFF');
+			$('#float-menu span').css('color', '#FFF');
+		};
+	}
+
 });
